a49e2483da39107e7eb1cc0d0ba529e875702d59
AlessandraYukimi <leyukimi@gmail.com>
2020-06-12 10:49:10 -0300

    Adicionando perfil do professor de balé

---
+++ perfil.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Professor</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet">    <link rel="stylesheet" href="./assets/css/perfilbale.css">
+    <script src="https://kit.fontawesome.com/049139915d.js" crossorigin="anonymous"></script>
+    
+</head>
+
+<body background="./assets/img/imagem1.png" class="backg">
+    <header>
+    <nav class="navbar text-primary navbar-expand-lg navbar-light">
+   
+   <a class="navbar-brand" href="index.php">
+       <img src="./assets/img/logo.jpeg" width="100" height="100">
+        
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+   </button>
+       <div class="collapse navbar-collapse" id="navbarNav">
+       
+       <ul class="navbar-nav ml-auto">
+           <li class="nav-item active">
+               
+               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="localhost/projeto-dh/comofunciona.php">Como funciona</a>
+           </li>
+           
+           <li class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+                   <a class="nav-link" href="#" data-toggle="dropdown" id="navDrop">Modalidades</a>
+           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" >
+               <a class="dropdown-item" href="#">Ballet</a>
+               <a class="dropdown-item" href="#">Contemporâneo</a>
+               <a class="dropdown-item" href="#">Dança de Salão</a>
+               <a class="dropdown-item" href="#">Dança do Ventre</a>
+               <a class="dropdown-item" href="#">Funk</a>
+               <a class="dropdown-item" href="#">Hip Hop/Breaking</a>
+               <a class="dropdown-item" href="#">Jazz</a>
+               <a class="dropdown-item" href="#">Samba</a>
+               <a class="dropdown-item" href="#">Sapateado</a>
+               <a class="dropdown-item" href="#">Vídeo Dance</a>
+               <a class="dropdown-item" href="#">Outros</a>
+           </div>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="#" data-toggle="modal" data-target="#modalLogin">Quero dar aulas</a>
+           </li>
+                         
+       </ul>
+   
+</nav>
+</header>
+
+<div class = título>
+  Saiba mais sobre o seu Professor!
+</div>
+
+<div id="divPai">;
+  <aside>
+    <h3>Professor Fulano</h3> 
+    <br>
+
+   
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active">
+    <img src="./assets/img/1.jpg" class="d-block w-100" alt="Foto1">
+    </li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1">
+    <img src="./assets/img/2.jpg" class="d-block w-100" alt="Foto2">
+    </li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2">
+    <img src="./assets/img/3.jpg" class="d-block w-100" alt="Foto3">
+    </li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="3">
+    <img src="./assets/img/4.jpg" class="d-block w-100" alt="Foto4">
+    </li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./assets/img/1.jpg" class="d-block w-100" alt="Foto1">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/2.jpg" class="d-block w-100" alt="Foto2">
+      <div class="carousel-caption d-none d-md-block">
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/3.jpg" class="d-block w-100" alt="Foto3">
+      <div class="carousel-caption d-none d-md-block">
+       </div>
+   </div>
+   <div class="carousel-item">
+      <img src="./assets/img/4.jpg" class="d-block w-100" alt="Foto4">
+      <div class="carousel-caption d-none d-md-block">
+        </div>
+   </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</br>
+
+<h6>Modalidade</h6>
+Balé <br><br>
+<h6>Dificuldade</h6>
+Iniciante / Intermediário / Avançado <br><br>
+<h6>Plataforma da aula</h6>
+Zoom / Google Meet<br><br>
+<h6>Redes sociais</h6>
+<a href="" target="_blank" rel="noopener" title="Acesse meu perfil no Facebook"><i class="fab fa-facebook fa-2x"></i></a>  <a href="" target="_blank" rel="noopener" title="Acesse meu perfil no Instagram"><i class="fab fa-instagram fa-2x"></i></a></p>
+
+</aside>
+<main>
+        <section>
+            <h3>Apresentação</h3></br>
+            <article>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates velit, dignissimos dolorum facere reiciendis enim aut sint reprehenderit, iste in sed nemo. Dicta rem nobis corrupti eaque eveniet nesciunt nostrum?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque debitis ullam facere quisquam reprehenderit odit? Voluptatum voluptas modi nostrum fuga sit vel. Iste odio excepturi eius veritatis? Ullam, debitis nulla.
+            </article>
+
+        </section>
+        
+</br></br></br>
+
+        <form>
+        <div class="form-row">
+                <h5 class="text-center pt-2 pb-0"> Entre em contato com o Professor!</h5>
+</div>
+  <div class="form-group col-md-10">
+    <label for="exampleFormControlInput1">Nome Completo</label>
+    <input type="nome" class="form-control" id="exampleFormControlInput1" placeholder="Preencha seu nome">
+  </div>
+  <div class="form-group col-md-10">
+    <label for="exampleFormControlInput2">Email</label>
+    <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="name@example.com">
+  </div>
+  
+  <div class="form-group col-md-10">
+    <label for="exampleFormControlTextarea1">Mensagem</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
+  </div>
+  <div class="form-group mt-4 mb-0 ml-3 d-flex flex-row justify-content-start">
+                <button type="reset" class="btn btn-secondary">Limpar</button>
+                <button type="submit" class="btn btn-primary ml-2">Enviar</button>
+            </div>
+</form>
+    </main>
+    </div>
+</br></br>
+    <footer class="page-footer font-small text-white bg-dark pt-4">
+
+  <div class="container">
+  <div class="container-fluid">
+      <ul class="text-center py-2">
+      <ul class="text-center">
+      <li class="list-inline-item">
+        <h5 class="mb-1">Você dá aula de dança on-line?</h5>
+      </li>
+      <li class="list-inline-item">
+        <a href="#!" class="btn btn-primary btn-rounded">Cadastre-se</a>
+      </li>
+    </ul>
+    <!-- Call to action -->
+  </div>
+    </div>
+</footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script> 
+    
+</body>
+</html>
