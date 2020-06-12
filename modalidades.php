068f619d372cd9dbdb0e9be2af3848df3e74d528
andreiagestaocultural@gmail.com <61522510+AndreiaFullStack@users.noreply.github.com>
2020-06-12 20:16:05 -0300

    fotos ballet

---
+++ modalidades.php
@@ -0,0 +1,68 @@
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?> 
+
+<head>
+<link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<header class="bg-danger text-white">
+    <div class="container text-center">
+      <h1>Ballet</h1>
+      <p class="lead">Escolha a/o professor(a): pronto, comece a praticar!</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+        <body>
+          <p class="lead">Ballet é um estilo de dança que se originou nas cortes da Itália renascentista 
+          durante o século XV, e que se desenvolveu ainda mais na Inglaterra, 
+          Rússia e França como uma forma de dança de concerto.</p>
+          <h2> Beneficios do Ballet </h2>
+          <ul>
+            <li>Coordenação motora</li>
+            <li>Equilibrio e auto conhecimento</li>
+            <li>Tonificação muscular</li>
+            <li>Bem estars</li>
+          </ul>
+       </div>
+  </section>
+
+
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="inc/img/ballet1.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Ballet para pessoas com deficiencia</h5>
+      <p class="card-text">Professora Fulana de tal, atua há mais de xx anos com um ballet que tras a 
+      inclusão, igualdade e auto estima. </p>
+      <p class="card-text"><small class="text-muted">Faça um aula experimental</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="inc/img/ballet2.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Ballet para todos os corpos</h5>
+      <p class="card-text">Mulheres gordas podem fazer aula de ballet sim! É uma excelente 
+      atividade física e vai te ajudar em tonificar seus musculos e manter sua saude e bem estar.</p>
+      <p class="card-text"><small class="text-muted">Faça uma aula experimental</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="inc/img/ballet3.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Quais as modalidades você pode fazer aqui?</h5>
+      <p class="card-text">Ballet Classico, Ballet Contemporâneo.</p>
+    <body>
+      
+    <footer>
+      <p class="card-text"><small class="text-muted">Faça uma aula experimental</small></p>
+    <footer>
+    </div>
+  </div>
+
+
+
+<?php require_once("./inc/footer.php"); ?>
